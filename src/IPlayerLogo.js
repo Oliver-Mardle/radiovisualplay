26bcdd5ac131a1bc49bcd4a94aebabc42787f76e
Oliver-Mardle <54029799+Oliver-Mardle@users.noreply.github.com>
2024-02-07 11:35:12 +0000

    Delete src directory

--- src/IPlayerLogo.js
+++
@@ -1,7 +0,0 @@
-import { Typography } from "@mui/material";
-
-export default function IPlayerLogo({ fontSize='4rem' }) {
-    return (
-        <Typography fontSize={fontSize} color={'#f54997'} marginLeft={5} marginTop={3}>iPLAYER</Typography>
-    );
-}
